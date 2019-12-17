9993511d6afee8ac2d45cbb68839ce9d7d12d260
maexx393 <manx393@gmail.com>
2019-12-17 11:59:32 +0200

    Update service-client.js

--- server/service-client.js
+++ server/service-client.js
@@ -31,8 +31,8 @@ function getTokenFromTokenEndoint (tokenEndpoint, user, password) {
     request.get(tokenEndpoint + TOKEN_PATH, {
       strictSSL: false,
       auth: {
-        'thabang@studex.io': user,
-        'maexx3935C': password
+        'ZF_xih1lrD0EG2XNjOrzIhNEaq6OMQDppy1b9ZBVdh0H': user,
+        'ZF_xih1lrD0EG2XNjOrzIhNEaq6OMQDppy1b9ZBVdh0H': password
       }
     }, function (err, res, body) {
       if (err) {
